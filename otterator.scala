def9cde1674db9d3b5f322c6b5582b3329f20403
sammoorhouse <sammoorhouse@10.211.55.3>
2016-03-11 23:00:12 +0000

    slightly nicer RLE-based otteration

--- otterator.scala
+++ otterator.scala
@@ -1,39 +1,48 @@
 package moorhouse{
   package _o{package t{package t{package e{object r{val `)` = "( "}}}}}  
   sealed trait otterretto{
+    def ***(c: Char, i:Int) = List.fill(i)(c).mkString
     def tter: String = this match{
-      case `o`(o) => ' ' + o.tter
-      case `to`(o) => '\'' + o.tter
-      case `tto`(o) => '\"' + o.tter
-      case `etto`(o) => '-' + o.tter
-      case `retto`(o) => '(' + o.tter
-      case `oretto`(o) => ')' + o.tter
-      case `toretto`(o) => '|' + o.tter
-      case `ttoretto`(o) => '/' + o.tter
-      case `ettoretto`(o) => '`' + o.tter
-      case `rettoretto`(o) => '.' + o.tter
-      case `orettoretto`(o) => ';' + o.tter
-      case `torettoretto`(o) => 'o' + o.tter
-      case `ttorettoretto`(o) => '0' + o.tter
-      case `ttorettorettor`(o) => '\\' + o.tter
-      case `ettorettorettor`(o) => "\n"
+      case `o`(o) => ' '.toString * o
+      case `to`(o) => '\''.toString * o
+      case `tto`(o) => '\"'.toString * o
+      case `etto`(o) => '-'.toString * o
+      case `retto`(o) => '('.toString * o
+      case `oretto`(o) => ')'.toString * o
+      case `toretto`(o) => '|'.toString * o
+      case `ttoretto`(o) => '/'.toString * o
+      case `ettoretto`(o) => '`'.toString * o
+      case `rettoretto`(o) => '.'.toString * o
+      case `orettoretto`(o) => ';'.toString * o
+      case `torettoretto`(o) => 'o'.toString * o
+      case `ttorettoretto`(o) => '0'.toString * o
+      case `ettorettoretto`(o) => '\\'.toString * o
+      case `rettorettoretto`(o) => '_'.toString * o
+      case `orettorettoretto`(o) => ','.toString * o
+      case `torettorettoretto`(o) => o./:("")((`o.t`,`t.e`) => `o.t`+`t.e`.tter)
     }
   }
-  case class `o`(o:otterretto) extends otterretto
-  case class `ttorettoretto`(o:otterretto) extends otterretto
-  case class `to`(o:otterretto) extends otterretto
-  case class `torettoretto`(o:otterretto) extends otterretto
-  case class `tto`(o:otterretto) extends otterretto
-  case class `orettoretto`(o:otterretto) extends otterretto
-  case class `etto`(o:otterretto) extends otterretto
-  case class `rettoretto`(o:otterretto) extends otterretto
-  case class `retto`(o:otterretto) extends otterretto
-  case class `ettoretto`(o:otterretto) extends otterretto
-  case class `oretto`(o:otterretto) extends otterretto
-  case class `ttoretto`(o:otterretto) extends otterretto
-  case class `toretto`(o:otterretto) extends otterretto
-  case class `ettorettoretto`(o:otterretto) extends otterretto
-  case class `ttorettorettor`(o:Char) extends otterretto
+  case class `torettorettoretto`(o:List[otterretto]) extends otterretto
+  case object `torettorettoretto`{
+    def apply(otter: otterretto*) = new `torettorettoretto`(otter.toList)
+  }
+  case class `orettorettoretto`(o: Int) extends otterretto
+  case class `rettorettoretto`(o: Int) extends otterretto
+  case class `ettorettoretto`(o: Int) extends otterretto
+  case class `ttorettoretto`(t: Int) extends otterretto
+  case class `torettoretto`(t: Int) extends otterretto
+  case class `orettoretto`(e: Int) extends otterretto
+  case class `rettoretto`(r: Int) extends otterretto
+  case class `ettoretto`(o: Int) extends otterretto
+  case class `ttoretto`(t: Int) extends otterretto
+  case class `toretto`(t: Int) extends otterretto
+  case class `oretto`(r: Int) extends otterretto
+  case class `retto`(r: Int) extends otterretto
+  case class `etto`(o: Int) extends otterretto
+  case class `tto`(t: Int) extends otterretto
+  case class `to`(t: Int) extends otterretto
+  case class `o`(e: Int) extends otterretto 
+
   /*
   """
                   |  /
@@ -51,11 +60,33 @@ package moorhouse{
   */
   object Otter
   {//jgs' otter, thanks
-    val line1 = `o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`toretto`(`o`(`o`(`ttoretto`(`ettorettoretto`('o')))))))))))))))))))))))
-    val line2 = `o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`rettoretto`(`etto`(`tto`(`to`(`tto`(`etto`(`rettoretto`(`o`(`o`(`o`(`toretto`(`o`(`ttoretto`(`ettorettoretto`('o'))))))))))))))))))))))
-    val line3 = `o`(`o`(`o`(`o`(`o`(`o`(`o`(`ttoretto`(`o`(`o`(`o`(`o`(`o`(`o`(`torettoretto`(`ttorettorettor`(`o`(`o`(`toretto`(`ttoretto`(`ettorettoretto`('o')))))))))))))))))))))
-        
+    val line1 = `torettorettoretto`(`o`(18), `toretto`(1), `o`(2), `ttoretto`(1))
+    val line2 = `torettorettoretto`(`o`(8), `rettoretto`(1), `etto`(1), `tto`(1), `to`(1), `tto`(1), `etto`(1), `rettoretto`(1), `o`(3), `toretto`(1), `o`(1), `ttoretto`(1))
+    val line3 = `torettorettoretto`(`o`(7), `ttoretto`(1), `o`(6), `torettoretto`(1), `ettorettoretto`(1), `o`(2), `ttoretto`(1))
+    val line4 = `torettorettoretto`(`o`(6), `toretto`(1), `o`(4), `torettoretto`(1), `o`(3), `ttorettoretto`(1), `oretto`(1), `rettoretto`(1), `etto`(1), `rettoretto`(1))
+    val line5 = `torettorettoretto`(`o`(6), `toretto`(1), `o`(7), `rettoretto`(1), `etto`(1), `orettoretto`(1), `retto`(1), `rettorettoretto`(1), `ttoretto`(1), `o`(5), `rettoretto`(1), `etto`(1), `rettoretto`(1))
+    val line6 = `torettorettoretto`(`o`(7), `ettorettoretto`(1), `o`(5), `ttoretto`(1), `o`(2), `ttoretto`(1), `oretto`(2), `rettoretto`(1), `etto`(3), `rettoretto`(1), `rettorettoretto`(1), `toretto`(1), `o`(2),`ettoretto`(1), `ettorettoretto`(1),`o`(2), `orettorettoretto`(1))
+    val line7 = `torettorettoretto`(`o`(8), `to`(1), `torettoretto`(1), `o`(2), `to`(1), `o`(2), `ttoretto`(1), `retto`(2), `o`(7), `ettoretto`(1), `to`(1), `etto`(1), `rettoretto`(1), `ttoretto`(1), `o`(1), `rettorettoretto`(1), `ttoretto`(1),`toretto`(1))
+    val line8 = `torettorettoretto`(`o`(10), `toretto`(1), `o`(2), `torettoretto`(1), `to`(1), `o`(2), `oretto`(2), `o`(8), `rettoretto`(1), `etto`(1), `rettoretto`(1), `orettoretto`(1), `ettoretto`(1), `o`(2), `ttoretto`(1))
+    val line9 = `torettorettoretto`(`o`(11), `to`(1), `torettoretto`(1), `o`(13), `toretto`(1), `o`(2), `ettoretto`(1),`toretto`(1), `etto`(1),`to`(1))
+    val line10 = `torettorettoretto`(`o`(13), `to`(1), `torettoretto`(1), `rettorettoretto`(1), `o`(8),  `etto`(1),`to`(1), `o`(4), `ttoretto`(1))
+    val line11 = `torettorettoretto`(`o`(16), `ettoretto`(2), `tto`(2), `etto`(2), `ettoretto`(1), `etto`(6),  `ettoretto`(1))
+    
+    println(line1.tter)
     println(line2.tter)
+    println(line3.tter)
+    println(line4.tter)
+    println(line5.tter)
+    println(line6.tter)
+    println(line7.tter)
+    println(line8.tter)
+    println(line9.tter)
+    println(line10.tter)
+    println(line11.tter)
+    //val line1 = `o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`toretto`(`o`(`o`(`ttoretto`(`ettorettoretto`('o')))))))))))))))))))))))
+    //val line2 = `o`(`o`(`o`(`o`(`o`(`o`(`o`(`o`(`rettoretto`(`etto`(`tto`(`to`(`tto`(`etto`(`rettoretto`(`o`(`o`(`o`(`toretto`(`o`(`ttoretto`(`ettorettoretto`('o'))))))))))))))))))))))
+    //val line3 = `o`(`o`(`o`(`o`(`o`(`o`(`o`(`ttoretto`(`o`(`o`(`o`(`o`(`o`(`o`(`torettoretto`(`ttorettorettor`(`o`(`o`(`toretto`(`ttoretto`(`ettorettoretto`('o')))))))))))))))))))))
+    //println(line3.tter)
     val (oo,t,e,r,toChar) = (32,18,2,65,10)
     val `t.t` = 50
     val `++` = " )"
