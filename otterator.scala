136c75c0b1e9c0b2709221c8402486f30f31e3bf
sammoorhouse <sammoorhouse@10.211.55.3>
2016-03-11 02:27:27 +0000

    further obfuscation

--- otterator.scala
+++ otterator.scala
@@ -1,18 +1,19 @@
 package moorhouse{
-  object Otter 
+  package _o{package t{package t{package e{object r{val `)` = "( "}}}}}  
+  object Otter
   {//jgs' otter, thanks
-    val (o,t,e,r,oter) = (32,18,2,65,10)
+    val (o,t,e,r,toChar) = (32,18,2,65,10)
     val `t.t` = 50
-    val `)` = "( "
     val `++` = " )"
     implicit class otterator(val `::`: String) {
       def otterate = {
         `::`.foldLeft(0)((`_`, _) => `_`.+(1)) match{
-          case l if (l <= o) => " " * (t - l/e)  + `)` + `::` + `++`
-          case l if (l <= r) => `)` + `::` + `++`
+          case ott if (ott <= o) => " " * (t - ott/e)  + _o.t.t.e.r.`)` + `::` + `++`
+          case er if (er <= r) => _o.t.t.e.r.`)` + `::` + `++`
           case l =>{
-            lazy val oterr : String => String = (s: String) => s.splitAt(`t.t`) match{
-              case (l, ls) => `)` + l + `++` + oter.toChar + (if(ls == "") oterr(ls) else "")
+            lazy val oterr : String => String = splitAt => splitAt.splitAt(`t.t`) match{
+              case (l, ls) => _o.t.t.e.r.`)` + l + `++` + 
+              (if(ls == "") "" else toChar.toChar +  oterr(ls))
             }
             oterr(`::`)
           }
