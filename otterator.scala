0add590cd391c4c147bb59172662ee671e7db9c2
sammoorhouse <sammoorhouse@10.211.55.3>
2016-03-12 17:52:16 +0000

    left-binding otterator

--- otterator.scala
+++ otterator.scala
@@ -1,78 +1,78 @@
 package moorhouse{
-  package _o{package t{package t{package e{object r{val `)` = "( "}}}}}  
-  sealed trait otterretto{
-    def tter: String = this match{
-      case `o`(o) => ' '.toString * o
-      case `to`(o) => '\''.toString * o
-      case `tto`(o) => '\"'.toString * o
-      case `etto`(o) => '-'.toString * o
-      case `retto`(o) => '('.toString * o
-      case `oretto`(o) => ')'.toString * o
-      case `toretto`(o) => '|'.toString * o
-      case `ttoretto`(o) => '/'.toString * o
-      case `ettoretto`(o) => '`'.toString * o
-      case `rettoretto`(o) => '.'.toString * o
-      case `orettoretto`(o) => ';'.toString * o
-      case `torettoretto`(o) => 'o'.toString * o
-      case `ttorettoretto`(o) => '0'.toString * o
-      case `ettorettoretto`(o) => '\\'.toString * o
-      case `rettorettoretto`(o) => '_'.toString * o
-      case `orettorettoretto`(o) => ','.toString * o
-      case `torettorettoretto`(o) => o./:("")((`o.t`,`t.e`) => `o.t`+`t.e`.tter)
-    }
-  }
-  case class `torettorettoretto`(o:List[otterretto]) extends otterretto
-  case object `torettorettoretto`{
-    def apply(otter: otterretto*) = new `torettorettoretto`(otter.toList)
-  }
-  case class `orettorettoretto`(o: Int) extends otterretto
-  case class `rettorettoretto`(o: Int) extends otterretto
-  case class `ettorettoretto`(o: Int) extends otterretto
-  case class `ttorettoretto`(t: Int) extends otterretto
-  case class `torettoretto`(t: Int) extends otterretto
-  case class `orettoretto`(e: Int) extends otterretto
-  case class `rettoretto`(r: Int) extends otterretto
-  case class `ettoretto`(o: Int) extends otterretto
-  case class `ttoretto`(t: Int) extends otterretto
-  case class `toretto`(t: Int) extends otterretto
-  case class `oretto`(r: Int) extends otterretto
-  case class `retto`(r: Int) extends otterretto
-  case class `etto`(o: Int) extends otterretto
-  case class `tto`(t: Int) extends otterretto
-  case class `to`(t: Int) extends otterretto
-  case class `o`(e: Int) extends otterretto 
-
-  object Otter
-  {//jgs' otter, thanks
-    val ootttteerr = List(`torettorettoretto`(`o`(18), `toretto`(1), `o`(2), `ttoretto`(1))
-    ,`torettorettoretto`(`o`(8), `rettoretto`(1), `etto`(1), `tto`(1), `to`(1), `tto`(1), `etto`(1), `rettoretto`(1), `o`(3), `toretto`(1), `o`(1), `ttoretto`(1))
-    ,`torettorettoretto`(`o`(7), `ttoretto`(1), `o`(6), `torettoretto`(1), `ettorettoretto`(1), `o`(2),`toretto`(1),  `ttoretto`(1))
-    ,`torettorettoretto`(`o`(6), `toretto`(1), `o`(4), `torettoretto`(1), `o`(3), `ttorettoretto`(1), `oretto`(1), `rettoretto`(1), `etto`(1), `rettoretto`(1))
-    ,`torettorettoretto`(`o`(6), `toretto`(1), `o`(7), `rettoretto`(1), `etto`(1), `orettoretto`(1), `retto`(1), `rettorettoretto`(1), `ttoretto`(1), `o`(5), `rettoretto`(1), `etto`(1), `rettoretto`(1))
-    ,`torettorettoretto`(`o`(7), `ettorettoretto`(1), `o`(5), `ttoretto`(1), `o`(2), `ttoretto`(1), `oretto`(2), `rettoretto`(1), `etto`(3), `rettoretto`(1), `rettorettoretto`(1), `toretto`(1), `o`(2),`ettoretto`(1), `ettorettoretto`(1),`o`(3), `orettorettoretto`(1))
-    ,`torettorettoretto`(`o`(8), `to`(1), `rettoretto`(1), `o`(2), `to`(1), `o`(2), `ttoretto`(1), `retto`(2), `o`(7), `ettoretto`(1), `to`(1), `etto`(1), `rettoretto`(1), `ttoretto`(1), `o`(1), `rettorettoretto`(1), `ttoretto`(1),`toretto`(1))
-    ,`torettorettoretto`(`o`(10), `toretto`(1), `o`(2), `rettoretto`(1), `to`(1), `o`(2), `oretto`(1), `o`(9), `rettoretto`(1), `etto`(1), `rettoretto`(1), `orettoretto`(1), `ettoretto`(1), `o`(1), `ttoretto`(1))
-    ,`torettorettoretto`(`o`(11), `to`(1), `rettoretto`(1), `o`(13), `toretto`(1), `o`(2), `ettoretto`(1),`ettorettoretto`(1), `etto`(1),`to`(1))
-    ,`torettorettoretto`(`o`(13), `to`(1), `rettoretto`(1), `rettorettoretto`(1), `o`(8),  `etto`(1),`to`(1), `o`(4), `ttoretto`(1))
-    ,`torettorettoretto`(`o`(16), `ettoretto`(2), `tto`(2), `etto`(2), `ettoretto`(1), `etto`(6),  `ettoretto`(1)))
-    
-    val (oo,t,e,r,toChar) = (32,18,2,65,10)
-    val `t.t` = 50
-    val `++` = " )"
-    implicit class otterator(val `::`: String) {
-      def otterate = {
-        `::`./:(0)((`_`, _) => `_`.+(1)) match{
-          case ott if (ott <= oo) => " " * (t - ott/e)  + _o.t.t.e.r.`)` + `::` + `++`
+	package _o{package t{package t{package e{object r{val `)` = "( "}}}}}  
+	object Otter
+	{
+		sealed trait otterretto{
+			def tter: String = this match{
+  			case `o`(o) => ' '.toString * o
+  			case `to`(o) => '\''.toString * o
+  			case `tto`(o) => '\"'.toString * o
+  			case `etto`(o) => '-'.toString * o
+  			case `retto`(o) => '('.toString * o
+  			case `oretto`(o) => ')'.toString * o
+  			case `toretto`(o) => '|'.toString * o
+  			case `ttoretto`(o) => '/'.toString * o
+  			case `ettoretto`(o) => '`'.toString * o
+  			case `rettoretto`(o) => '.'.toString * o
+  			case `orettoretto`(o) => ';'.toString * o
+  			case `torettoretto`(o) => 'o'.toString * o
+  			case `ttorettoretto`(o) => '0'.toString * o
+  			case `ettorettoretto`(o) => '\\'.toString * o
+  			case `rettorettoretto`(o) => '_'.toString * o
+  			case `orettorettoretto`(o) => ','.toString * o
+  			case `torettorettoretto`(o) => o./:("")((`o.t`,`t.e`) => `o.t`+`t.e`.tter)
+  		}
+		}
+  	case class `torettorettoretto`(o:List[otterretto]) extends otterretto
+  	case object `torettorettoretto`{
+  		def apply(otter: otterretto*) = new `torettorettoretto`(otter.toList)
+  	}
+  	case class `orettorettoretto`(o: Int) extends otterretto
+  	case class `rettorettoretto`(o: Int) extends otterretto
+  	case class `ettorettoretto`(o: Int) extends otterretto
+  	case class `ttorettoretto`(t: Int) extends otterretto
+  	case class `torettoretto`(t: Int) extends otterretto
+  	case class `orettoretto`(e: Int) extends otterretto
+  	case class `rettoretto`(r: Int) extends otterretto
+  	case class `ettoretto`(o: Int) extends otterretto
+  	case class `ttoretto`(t: Int) extends otterretto
+  	case class `toretto`(t: Int) extends otterretto
+  	case class `oretto`(r: Int) extends otterretto
+  	case class `retto`(r: Int) extends otterretto
+  	case class `etto`(o: Int) extends otterretto
+  	case class `tto`(t: Int) extends otterretto
+  	case class `to`(t: Int) extends otterretto
+  	case class `o`(e: Int) extends otterretto 
+    val two= new `ettoretto`(1).tter.andThen { x => 1 }(0)
+  	//jgs' otter, thanks
+  	val ootttteerr = List(`torettorettoretto`(`o`(18), `toretto`(two), `o`(2), `ttoretto`(1))
+  			,`torettorettoretto`(`o`(8), `rettoretto`(two), `etto`(two), `tto`(two), `to`(1), `tto`(1), `etto`(1), `rettoretto`(1), `o`(3), `toretto`(1), `o`(1), `ttoretto`(1))
+  			,`torettorettoretto`(`o`(7), `ttoretto`(1), `o`(6), `torettoretto`(two), `ettorettoretto`(1), `o`(2),`toretto`(1),  `ttoretto`(1))
+  			,`torettorettoretto`(`o`(6), `toretto`(1), `o`(4), `torettoretto`(two), `o`(3), `ttorettoretto`(two), `oretto`(1), `rettoretto`(two), `etto`(1), `rettoretto`(1))
+  			,`torettorettoretto`(`o`(6), `toretto`(1), `o`(7), `rettoretto`(1), `etto`(1), `orettoretto`(1), `retto`(1), `rettorettoretto`(1), `ttoretto`(1), `o`(5), `rettoretto`(1), `etto`(1), `rettoretto`(1))
+  			,`torettorettoretto`(`o`(7), `ettorettoretto`(1), `o`(5), `ttoretto`(1), `o`(2), `ttoretto`(1), `oretto`(2), `rettoretto`(1), `etto`(3), `rettoretto`(1), `rettorettoretto`(1), `toretto`(1), `o`(2),`ettoretto`(1), `ettorettoretto`(1),`o`(3), `orettorettoretto`(1))
+  			,`torettorettoretto`(`o`(8), `to`(1), `rettoretto`(two), `o`(2), `to`(1), `o`(2), `ttoretto`(two), `retto`(2), `o`(7), `ettoretto`(1), `to`(1), `etto`(1), `rettoretto`(1), `ttoretto`(1), `o`(1), `rettorettoretto`(1), `ttoretto`(1),`toretto`(1))
+  			,`torettorettoretto`(`o`(10), `toretto`(1), `o`(2), `rettoretto`(1), `to`(1), `o`(2), `oretto`(1), `o`(9), `rettoretto`(1), `etto`(1), `rettoretto`(1), `orettoretto`(1), `ettoretto`(1), `o`(1), `ttoretto`(1))
+  			,`torettorettoretto`(`o`(11), `to`(1), `rettoretto`(1), `o`(13), `toretto`(two), `o`(2), `ettoretto`(two),`ettorettoretto`(1), `etto`(1),`to`(1))
+  			,`torettorettoretto`(`o`(13), `to`(two), `rettoretto`(two), `rettorettoretto`(1), `o`(8),  `etto`(1),`to`(1), `o`(4), `ttoretto`(1))
+  			,`torettorettoretto`(`o`(16), `ettoretto`(2), `tto`(2), `etto`(2), `ettoretto`(1), `etto`(6),  `ettoretto`(1)))
+  
+  	val (oo,t,e,r,toChar) = (32,18,2,65,10)  
+  	val `t.t` = 50
+  	val `++` = " )"
+  	implicit class otterator(val `::`: String) {
+  		def otterate = { `::`./:(0)((`_`, _) => `_`.+(two)) match{
+  				case ott if (ott <= oo) => " " * (t - ott/e)  + _o.t.t.e.r.`)` + `::` + `++`
           case er if (er <= r) => _o.t.t.e.r.`)` + `::` + `++`
           case l =>{
-            lazy val oterr : String => String = splitAt => splitAt.splitAt(`t.t`) match{
-              case (l, ls) => _o.t.t.e.r.`)` + l + `++` + 
-              (if(ls == "") "" else toChar.toChar +  oterr(ls))
-            }
-            oterr(`::`)
+          	lazy val oterr : String => String = splitAt => splitAt.splitAt(`t.t`) match{
+          	  case (l, ls) => _o.t.t.e.r.`)` + l + `++` + 
+                (if(ls == "") "" else toChar.toChar +  oterr(ls))
+          	}
+  	        oterr(`::`)
           }
-        }
-      }.++((ootttteerr map (o => o.tter)).foldLeft("")(_+"\n"+_) )
-    }
-  }
+  		  }
+  		}.++((ootttteerr map (o => o.tter))./:("")(_+toChar.toChar+_) )
+  	}
+	}
 }
